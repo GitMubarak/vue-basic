be93290005d0076986482648bed3ea64b08822be
Md. Hossni Mubarak <mhmrajib@gmail.com>
2020-04-12 17:58:39 +0600

    Mousemove, mouseover and mouseout event example

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var myview = new Vue({
     el: "#my-app",
     data:{
         counter: 0,
+        x: 0,
+        y: 0,
+        mouse: "Mouse",
     },
     methods:{
         increment: function(val) {
@@ -10,5 +13,15 @@ var myview = new Vue({
         decrement: function(val) {
             this.counter -= val;
         },
+        movefunction: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        overfunction: function() {
+            this.mouse = 'Mouse is over';
+        },
+        outfunction: function() {
+            this.mouse = 'Mouse is out';
+        }
     }
 });
